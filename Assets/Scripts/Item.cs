2d738a04d4058307663eb8246ac5c7e098f77d21
Matvey Krotikov <hopAhop96@gmail.com>
2022-07-06 00:48:53 +0300

    05.07.2022

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu(menuName = "Grinchi/Item")]
+
+public sealed class Item : ScriptableObject
+{
+    public int value;
+
+    public Sprite sprite;
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class Tile : MonoBehaviour
+{
+    public int x;
+    public int y;
+
+    public Item item;
+
+    public Image icon;
+
+    public Button button;
+}
