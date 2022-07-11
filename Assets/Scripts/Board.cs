e66cca70d3145f304942f765203668d529d67159
Matvey Krotikov <hopAhop96@gmail.com>
2022-07-11 18:06:19 +0300

    11.07.2022

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -13,6 +13,7 @@ public sealed class Board : MonoBehaviour
     public int Width => Tiles.GetLength(dimension: 0);
     public int Height => Tiles.GetLength(dimension: 1);
     private readonly List<Tile> _selection = new List<Tile>();
+    private const float TweenkyDuration = 0.25f;
 
 
     private void Awake() => Instance = this;
@@ -33,14 +34,30 @@ public sealed class Board : MonoBehaviour
 
         }   
     } 
-    public void Select(Tile tile)
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.A)) return;
+        foreach (var connectedTile in Tiles[0,0].GetConnectedTiles()) connectedTile.icon.transform.DOScale(1.25f, TweenkyDuration).Play();    
+    }
+    public async void Select(Tile tile)
     {
         if (!_selection.Contains(tile)) _selection.Add(tile);
         if (_selection.Count < 2) return;
         Debug.Log($"Selected tiles at ({_selection[0].x}, {_selection[0].y}) and ({_selection[1].x}, {_selection[1].y})");
+        await Swap(_selection[0], _selection[1]);
+
+        if (CanPop())
+        {
+            Pop();
+        }
+        else
+        {
+            await Swap(_selection[0], _selection[1]);
+        }
         _selection.Clear();
     }
-    // Работает параллельно, мы ожидаем 
+    // Parallel thread, we are waiting
     public async Task Swap(Tile tile1, Tile tile2)
     {
         var icon1 = tile1.icon;
@@ -48,10 +65,60 @@ public sealed class Board : MonoBehaviour
 
         var icon1Transform = icon1.transform;
         var icon2Transform = icon2.transform;
-    // Анимация при перемещении элементов с одного места на другое. Используем ассет DOTween.
+    // DOTween asset for animation 
         var sequence = DOTween.Sequence();
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenkyDuration))
+            .Join(icon2Transform.DOMove(icon1Transform.position, TweenkyDuration));
         await sequence.Play()
             .AsyncWaitForCompletion();
-    }
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
+        tile1.icon = icon2;
+        tile2.icon = icon1;
 
+        var tile1Item = tile1.Item;
+        tile1.Item = tile2.Item;
+        tile2.Item = tile1Item;
+
+    }
+    private bool CanPop()
+    {
+        for (var y = 0; y < Height; y++) 
+            for (var x = 0; x < Width; x++)
+                if (Tiles[x, y].GetConnectedTiles().Skip(1).Count() >= 2)
+                    return true;
+                return false;
+    }
+    private async void Pop()
+    {
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var tile = Tiles[x, y];
+
+                var connectedTiles  = tile.GetConnectedTiles();
+
+                if (connectedTiles.Skip(1).Count() < 2) continue;
+
+                var defSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles) defSequence.Join(connectedTile.icon.transform.DOScale(Vector3.zero, TweenkyDuration));
+
+                await defSequence.Play()
+                    .AsyncWaitForCompletion();
+
+                var infSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles)
+                { 
+                    connectedTile.Item = ItemDatabase.Items[Random.Range(0, ItemDatabase.Items.Length)];
+
+                    infSequence.Join(connectedTile.icon.transform.DOScale(Vector3.one, TweenkyDuration));
+                }
+                await infSequence.Play()
+                    .AsyncWaitForCompletion();
+            }
+        }
+    } 
 }
